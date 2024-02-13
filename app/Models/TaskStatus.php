41bfb005f905a86d849d2896790a10eb7d854971
Robi Wanda <robiwanda@orderonline.id>
2024-02-14 00:07:13 +0700

    create migration task status

---
+++ app/Models/TaskStatus.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TaskStatus extends Model
+{
+    use SoftDeletes, HasFactory;
+
+	protected $table = 'task_statuses';
+
+	protected $dates = ['deleted_at'];
+
+	protected $primaryKey = 'id';
+
+	protected $fillable = [
+		'name',
+	];
+
+	public function setAttributeFromJson($attributes)
+	{
+		if (isset($attributes['name'])) {
+			$this->name = $attributes['name'];
+		}
+	}
+}
